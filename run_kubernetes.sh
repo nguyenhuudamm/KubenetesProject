d2223115b0622aa8fabe92b84d67c30eb0ee2e0f
nguyenhuudamm <nguyenhuudam98@gmail.com>
2023-09-17 21:46:11 +0700

    ccc

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 # This tags and uploads an image to Docker Hub
-# setting cluster
-# eksctl create cluster --name kubernitesproject --region=us-east-1
-# eksctl get cluster --name=kubernitesproject --region=us-east-1
+
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
@@ -25,21 +23,46 @@ kubectl get pods
 # Forward the container port to host
 kubectl port-forward flaskskearlndemo 8000:80
 
-aws eks update-kubeconfig --region us-east-1 --name kubernitesproject
-aws eks update-kubeconfig --name kubernitesproject --region us-east-1 --profile default
+
 
 export KUBECONFIG=$KUBECONFIG:/home/ec2-user/.kube/config
-echo 'export KUBECONFIG=$KUBECONFIG:/home/ec2-user/.kube/config' >> ~/.bashrc
+export KUBECONFIG=/home/ec2-user/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+kubectl config view --raw > ~/.kube/admin.conf
+cp /etc/kubernetes/admin.conf $HOME/
+chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+
+echo 'export KUBECONFIG=$HOME/admin.conf' >> $HOME/.bashrc
+echo 'export KUBECONFIG=$HOME/.kube/config' >> $HOME/.bashrc
 
 kubectl config use-context arn:aws:eks:us-east-1:830989663524:cluster/kubernitesproject
 
 kubectl config set-credentials <user-name> --username=<your-username> --password=<your-password>
 
-aws eks describe-cluster --name kubernitesproject --region us-east-1
+kubectl config set-credentials my-user --token=your-token
+
+aws eks describe-cluster --name kubernetesproject --region us-east-1
+
+# setting cluster
+#create:  
+    eksctl create cluster --name kubernetesproject --region=us-east-1
+#cleanup: 
+    eksctl delete cluster --region=us-east-1 --name=kubernetesproject
+eksctl get cluster --name=kubernitesproject --region=us-east-1
 
 # lệnh xác định IAM của aws 
- aws sts get-caller-identity
- 
+aws sts get-caller-identity
+# lệnh xác định IAM của user tạo cluster
+aws eks describe-cluster --name kubernetesproject
+# update
+aws eks update-kubeconfig --region us-east-1 --name kubernitesproject
+aws eks update-kubeconfig --region us-east-1 --name kubernetesproject --profile default
+# xac nhận rằng tệp kubeconfig đã được cập nhật, hãy chạy lệnh sau:
+kubectl config view --minify
+
+
  kubectl -n kube-system edit configmap aws-auth
 
  
