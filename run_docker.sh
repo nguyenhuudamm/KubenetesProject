3f91b419400170f1c4f67a73118408f72d5ada57
nguyenhuudamm <nguyenhuudam98@gmail.com>
2023-09-30 15:54:15 +0700

    aaa

--- run_docker.sh
+++ run_docker.sh
@@ -20,7 +20,6 @@ docker ps
 
 # Step 5:
 # Get IP address
-docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' <container-id>
-
-docker run -it --network container:19ad54b7d4d3 ubuntu:latest
+# docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' <container-id>
+# docker run -it --network container:19ad54b7d4d3 ubuntu:latest
 
